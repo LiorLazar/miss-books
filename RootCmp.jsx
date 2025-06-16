8b4a6febde92e05d783b88ac066c5b7f41eff60e
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-16 12:04:15 +0300

    Add RootCmp.jsx

---
+++ RootCmp.jsx
@@ -0,0 +1,15 @@
+const { useState } = React
+
+export function RootCmp() {
+
+    const [page, setPage] = useState(null)
+
+    return (
+        <section className="app">
+
+            <main>
+
+            </main>
+        </section>
+    )
+}
