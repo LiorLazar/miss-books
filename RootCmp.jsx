02c9b7ff3128d2c54dd9d40192ff9952fd11b9b5
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-16 12:10:02 +0300

    Add Home Page

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,14 +1,16 @@
+import { Home } from "./pages/Home.jsx"
+
 const { useState } = React
 
 export function RootCmp() {
 
-    const [page, setPage] = useState(null)
+    const [page, setPage] = useState('home')
 
     return (
         <section className="app">
 
             <main>
-
+                {page == 'home' && <Home />}
             </main>
         </section>
     )
