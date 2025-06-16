1e67a82fe9cd822d619ab5ccebb500b877480d01
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-16 15:21:54 +0300

    Add Books Filter

---
+++ components/BookFilter.jsx
@@ -0,0 +1,40 @@
+const { useState, useEffect } = React
+
+export function BookFilter({ defaultFilter, onSetFilter }) {
+    const [filterByToEdit, setFilterByToEdit] = useState({ ...defaultFilter })
+
+    useEffect(() => {
+        onSetFilter(filterByToEdit)
+    }, [filterByToEdit])
+
+    function handleChange({ target }) {
+        const field = target.name
+        let value = target.value
+        switch (target.type) {
+            case 'number':
+            case 'range':
+                value = +value
+                break;
+
+            case 'checkbox':
+                value = target.checked
+                break;
+        }
+        setFilterByToEdit(prevFilter => ({ ...prevFilter, [field]: value }))
+    }
+
+    const { txt, minSpeed } = filterByToEdit
+    return (
+        <section className="book-filter container">
+            <h2>Filter Our Books</h2>
+
+            <form>
+                <label htmlFor="txt">Vendor</label>
+                <input onChange={handleChange} value={txt} name="txt" id="txt" type="text" />
+
+                <label htmlFor="txt">Min Speed</label>
+                <input onChange={handleChange} value={minSpeed || ''} name="minSpeed" id="minSpeed" type="number" />
+            </form>
+        </section>
+    )
+}
