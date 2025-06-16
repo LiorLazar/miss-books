82766a8bb471315cfd3b83250e68f6b6f6cd8d6a
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-16 15:05:02 +0300

    Add Book Index

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,5 +1,53 @@
+import { BookList } from "../components/BookList.jsx"
+import { bookService } from "../services/book.service.js"
+
+const { useState, useEffect, Fragment } = React
+
 export function BookIndex() {
+
+    const [books, setBooks] = useState(null)
+    const [selectedBookId, setSelectedBookId] = useState(null)
+    const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
+
+    useEffect(() => {
+        loadBooks()
+    }, [filterBy])
+
+    function loadBooks() {
+        bookService.query(filterBy)
+            .then(books => setBooks(books))
+            .catch(err => console.log('err:', err))
+    }
+
+    function onRemoveBook(bookId) {
+        bookService.remove(bookId)
+            .then(() => {
+                setBooks(books => books.filter(book => book.id !== bookId))
+            })
+            .catch(err => console.log('err:', err))
+    }
+
+    function onSelectedBookId(bookId) {
+        setSelectedBookId(bookId)
+    }
+
+    function onSetFilter(bodokId) {
+        setFilterBy(prevFilter => ({ ...prevFilter, ...filterBy }))
+    }
+
+    if (!books) return <div>Loading...</div>
+
     return (
-        <section className="book-index"></section>
+        <section className="book-index">
+            {!selectedBookId &&
+                <Fragment>
+                    <BookList
+                        books={books}
+                        onRemoveBook={onRemoveBook}
+                        onSelectBookId={onSelectedBookId}
+                    />
+                </Fragment>
+            }
+        </section>
     )
 }
