d1bdd91894f1680cee61aa3becfe65ef7e42e428
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-16 12:37:55 +0300

    Fix Image Bug

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,7 +3,7 @@ export function Home() {
     return (
         <section className="home container">
             <h1>Book's R Us!</h1>
-            <img src="../assets/img/react.png" alt="hero-image" />
+            <img src="./assets/img/react.png" alt="hero-image" />
         </section>
     )
 }
