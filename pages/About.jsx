056d3f7feaecdbb5200974d182bc9ab1e0a5cfdb
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-17 14:01:09 +0300

    Implement LongText Cmp in About

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,13 +1,14 @@
+import { LongText } from "../components/LongText.jsx"
+
 export function About() {
 
+    const txt = ` Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Optio dolore sapiente, iste animi corporis nisi atque tempora assumenda dolores.
+                Nobis nam dolorem rerum illo facilis nemo sit voluptatibus laboriosam necessitatibus!`
     return (
         <section className="about container">
             <h1>About books and so...</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Optio dolore sapiente, iste animi corporis nisi atque tempora assumenda dolores.
-                Nobis nam dolorem rerum illo facilis nemo sit voluptatibus laboriosam necessitatibus!
-            </p>
+            <LongText txt={txt} />
         </section>
     )
 }
