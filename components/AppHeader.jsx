020f57bb7c3966647514c05f0244f38ae6e0ccf0
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-18 10:24:08 +0300

    Improve App Header

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,14 +1,18 @@
 
 export function AppHeader({ onSetPage }) {
 
+    const links = [
+        { path: 'home' }, { path: 'about' }, { path: 'book' }
+    ]
     return (
         <header className="app-header container">
             <section>
                 <h1>Miss Books</h1>
                 <nav className="app-nav">
-                    <a onClick={() => onSetPage('home')}>Home</a>
+                    {links.map(link => <a key={link.path} onClick={() => onSetPage(link.path)}>{link.path}</a>)}
+                    {/* <a onClick={() => onSetPage('home')}>Home</a>
                     <a onClick={() => onSetPage('about')}>About</a>
-                    <a onClick={() => onSetPage('book')}>Books</a>
+                    <a onClick={() => onSetPage('book')}>Books</a> */}
                 </nav>
             </section>
         </header>
