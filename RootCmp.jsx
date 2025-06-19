568c0aba5156fa1481ef37feff916517524f2e93
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-19 12:57:03 +0300

    Implement Basic Routing

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,19 +4,25 @@ import { BookIndex } from "./pages/BookIndex.jsx"
 import { Home } from "./pages/Home.jsx"
 
 const { useState } = React
+const Router = ReactRouterDOM.HashRouter
+
+const { Routes, Route, Navigate } = ReactRouterDOM
 
 export function RootCmp() {
 
-    const [page, setPage] = useState('book')
-
     return (
-        <section className="app">
-            <AppHeader onSetPage={page => setPage(page)} />
-            <main>
-                {page == 'home' && <Home />}
-                {page == 'about' && <About />}
-                {page == 'book' && <BookIndex />}
-            </main>
-        </section>
+        <Router>
+            <section className="app">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<Navigate to="/home" />} />
+                        <Route path='/home' element={<Home />} />
+                        <Route path='/about' element={<About />} />
+                        <Route path='/book' element={<BookIndex />} />
+                    </Routes>
+                </main>
+            </section>
+        </Router>
     )
 }
