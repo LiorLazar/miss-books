42f1e21c4a4b2ae08a46d36c24bd26857659cdb2
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-20 13:00:50 +0300

    Improve Book Details

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -23,9 +23,9 @@ export function RootCmp() {
                         <Route path='/about' element={<About />} />
                         <Route path='/book' element={<BookIndex />} />
 
-                        <Route path='/book/:carId' element={<BookDetails />} />
+                        <Route path='/book/:bookId' element={<BookDetails />} />
                         <Route path='/book/edit' element={<BookEdit />} />
-                        <Route path='/book/edit/:carId' element={<BookEdit />} />
+                        <Route path='/book/edit/:bookId' element={<BookEdit />} />
                     </Routes>
                 </main>
             </section>
