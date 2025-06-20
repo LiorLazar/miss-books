0403a6fdb18601a955028d3b2dd6400bd750d0ca
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-20 13:05:57 +0300

    Implement setNextPrevBookId function

--- services/book.service.js
+++ services/book.service.js
@@ -39,6 +39,7 @@ function query(filterBy = {}) {
 
 function get(bookId) {
     return storageService.get(BOOK_KEY, bookId)
+        .then(_setNextPrevBookId)
 }
 
 function remove(bookId) {
@@ -102,4 +103,15 @@ function _createBooks() {
         saveToStorage(BOOK_KEY, books)
     }
     console.log('books', books)
+}
+
+function _setNextPrevBookId(book) {
+    return query().then((books) => {
+        const bookIdx = books.findIndex((currBook) => currBook.id === book.id)
+        const nextBook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
+        const prevBook = books[bookIdx - 1] ? books[bookIdx - 1] : books[books.length - 1]
+        book.nextBookId = nextBook.id
+        book.prevBookId = prevBook.id
+        return book
+    })
 }
