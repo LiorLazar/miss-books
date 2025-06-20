82e3f079c7991f5676246309241c3966f49d6ae3
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-06-20 12:20:50 +0300

    Fix Book index Routing

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,27 +1,22 @@
 import { BookFilter } from "../components/BookFilter.jsx"
 import { BookList } from "../components/BookList.jsx"
 import { bookService } from "../services/book.service.js"
-import { BookDetails } from "./BookDetails.jsx"
-import { BookEdit } from "./BookEdit.jsx"
 
+const { Link } = ReactRouterDOM
 const { useState, useEffect, Fragment } = React
 
 export function BookIndex() {
 
     const [books, setBooks] = useState(null)
-    const [selectedBookId, setSelectedBookId] = useState(null)
-    const [bookIdToEdit, setBookIdToEdit] = useState(null)
     const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
 
     useEffect(() => {
-        console.log('loading books');
-
         loadBooks()
     }, [filterBy])
 
     function loadBooks() {
         bookService.query(filterBy)
-            .then(books => setBooks(books))
+            .then(setBooks)
             .catch(err => console.log('err:', err))
     }
 
@@ -30,58 +25,21 @@ export function BookIndex() {
             .then(() => {
                 setBooks(books => books.filter(book => book.id !== bookId))
             })
-            .catch(err => {
-                console.log('err:', err)
-            })
+            .catch(err => console.log('err:', err))
     }
 
-    function onSetFilter(filterBy) { // ex: {txt:'asd'}
+    function onSetFilterBy(filterBy) { // ex: {txt:'asd'}
         setFilterBy(prevFilter => ({ ...prevFilter, ...filterBy }))
     }
 
-    function onSelectBookId(bookId) {
-        setSelectedBookId(bookId)
-    }
-
-    function onBookIdToEdit(bookId) {
-        console.log(bookId);
-
-        setBookIdToEdit(bookId)
-    }
-
-    // BONUS: create Suspense CMP
-    if (!books) return <div>Loading...</div>
-
-    if (bookIdToEdit) {
-        return (
-            <BookEdit
-                bookId={bookIdToEdit}
-                onBack={() => setBookIdToEdit(null)} />
-        )
-    }
-
+    if (!books) return <div className="container">Loading...</div>
     return (
         <section className="book-index">
-            {selectedBookId &&
-                <BookDetails
-                    bookId={selectedBookId}
-                    onBack={() => setSelectedBookId(null)}
-                />
-            }
-            {!selectedBookId &&
-                <Fragment>
-                    <BookFilter
-                        defaultFilter={filterBy}
-                        onSetFilter={onSetFilter}
-                    />
-                    <BookList
-                        books={books}
-                        onRemoveBook={onRemoveBook}
-                        onSelectBookId={onSelectBookId}
-                        onBookIdToEdit={onBookIdToEdit}
-                    />
-                </Fragment>
-            }
+            <BookFilter onSetFilterBy={onSetFilterBy} filterBy={filterBy} />
+            <section className="container">
+                <Link to="/book/edit">Add</Link>
+            </section>
+            <BookList onRemoveBook={onRemoveBook} books={books} />
         </section>
     )
 }
